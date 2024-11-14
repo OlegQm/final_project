4c556e6d89a2dd00c64cc462a3bdc4c8b8c53da6
olegqm <olegqm@gmail.com>
2024-11-14 16:15:50 +0100

    'Next page' button's logic updated, autoredirect added

--- files/js/second-level-js.js
+++ files/js/second-level-js.js
@@ -1,1 +1,7 @@
+previousLevel = "../../../index.html";
+const previousLevelButton = document.getElementById("previousLevelButton");
+previousLevelButton.addEventListener("click", () => {
+    window.location.href = previousLevel;
+});
+
 // TODO
